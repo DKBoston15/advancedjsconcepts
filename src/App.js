1b7f7294b6fc677e36837eac85c122f916cc152f
DKBoston15 <46634008+DKBoston15@users.noreply.github.com>
2021-01-02 17:09:19 -0800

    Theme Toggle

--- src/App.js
+++ src/App.js
@@ -1,13 +1,65 @@
 import React, { useState } from "react";
 import "./App.css";
 import MainMenu from "./components/MainMenu";
+import { withStyles } from "@material-ui/core/styles";
 import MainContentContainer from "./components/MainContentContainer";
+import Switch from "@material-ui/core/Switch";
 
 function App() {
     const [content, setContent] = useState("Async");
+    const [state, setState] = React.useState({
+        checkedA: true,
+        checkedB: true
+    });
+
+    const handleChange = (event) => {
+        setState({ ...state, [event.target.name]: event.target.checked });
+        console.log(state);
+        if (state.checkedA === true) {
+            //lightmode
+            document.documentElement.style.setProperty("--main", "#131313");
+            document.documentElement.style.setProperty(
+                "--secondary",
+                "#2196f3"
+            );
+            document.body.style.backgroundColor = "white";
+        } else {
+            document.documentElement.style.setProperty("--main", "white");
+            document.documentElement.style.setProperty("--secondary", "orange");
+            document.body.style.backgroundColor = "#131313";
+        }
+    };
+
+    const PurpleSwitch = withStyles({
+        switchBase: {
+            color: "#2196f3",
+            "&$checked": {
+                color: "orange"
+            },
+            "& + $track": {
+                backgroundColor: "#2196f3"
+            },
+            "&$checked + $track": {
+                backgroundColor: "orange"
+            }
+        },
+        checked: {},
+        track: {}
+    })(Switch);
+
     return (
         <div className="App">
-            <h1 className="header">Advanced JS Concepts</h1>
+            <div className="headerContainer">
+                <h1 className="header">Advanced JS Concepts</h1>
+                <PurpleSwitch
+                    checked={state.checkedA}
+                    onChange={handleChange}
+                    name="checkedA"
+                    inputProps={{ "aria-label": "secondary checkbox" }}
+                    className="themeSwitch"
+                />
+            </div>
+
             <MainMenu setContent={setContent} />
             <MainContentContainer content={content} />
         </div>
